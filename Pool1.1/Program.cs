e9c5a2f6f1d38d1f8f2809842636b82e19e6dbcb
Cociuba Nicusor <cociubanicusor66@gmail.com>
2020-12-08 20:46:28 +0200

    1

---
+++ Pool1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Numere1
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            //Rezolvati ecuatia de gradul 1 cu o necunoscuta: ax+b = 0, unde a si b sunt date de intrare. 
+            int a, b;
+            float x;
+            Console.WriteLine("Introduceti coeficientul lui x.");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti termenul liber.");
+            b = int.Parse(Console.ReadLine());
+            if (a == 0)
+                Console.WriteLine("Ecuatia nu exista.");
+            else
+                Console.WriteLine($"Solutia ecuatiei este {x = -b / a}.");
+            Console.ReadKey();
+        }
+    }
+}
