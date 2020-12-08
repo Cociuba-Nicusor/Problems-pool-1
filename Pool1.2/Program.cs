731a081666c4322cf7894bf111eac7cf07d08236
Cociuba Nicusor <cociubanicusor66@gmail.com>
2020-12-08 20:50:55 +0200

    2

---
+++ Pool1.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pool1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
