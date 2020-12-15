6ef3d6578adc4cfec0cd274c1002db5672061009
Cociuba Nicusor <cociubanicusor66@gmail.com>
2020-12-15 13:56:34 +0200

    ty

---
+++ Pool1.6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pool1._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Detreminati daca trei numere pozitive a, b si c pot fi lungimile laturilor unui triunghi. 
+            double a, b, c;
+            Console.WriteLine("Introduceti numarul a:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti numarul b:");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti numarul c:");
+            c = int.Parse(Console.ReadLine());
+            if (a < b + c && b < a + c && c < a + b)
+            {
+                Console.WriteLine($"Numerele {a},{b},{c} pot forma laturile unui triunghi.");
+            }
+            else
+            {
+                Console.WriteLine($"Numerele {a},{b},{c} nu formeaza laturile unui triunghi.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
