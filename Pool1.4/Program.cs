303bc2ea7a00f30799eb9eb98c8b5f92937ac3bc
Cociuba Nicusor <cociubanicusor66@gmail.com>
2020-12-15 13:43:54 +0200

    ty

---
+++ Pool1.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Pool1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Detreminati daca un an y este an bisect.
+            int an;
+            Console.WriteLine("Introduceti anul:");
+            an = int.Parse(Console.ReadLine());
+            if (an % 4 == 0 && an % 100 != 0 || an % 400 == 0) 
+            {
+                Console.WriteLine($"Anul {an} este bisect.");
+            }
+            else
+            {
+                Console.WriteLine($"Anul {an} nu este bisect.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
