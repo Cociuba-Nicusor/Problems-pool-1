9b7a8321c8994902741c08af5febe106f7b31786
Cociuba Nicusor <cociubanicusor66@gmail.com>
2020-12-17 21:51:32 +0200

    ty

---
+++ Pool1.13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pool1._13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Determianti cati ani bisecti sunt intre anii y1 si y2.
+            int y1, y2, i, n=0;
+            Console.WriteLine("Introduceti primul an:");
+            y1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti ultimul an:");
+            y2 = int.Parse(Console.ReadLine());
+            for(i=y1;i<=y2;i++)
+            {
+                if (i % 4 == 0 && i % 100 == 0  i % 400 == 0)
+                    n++;
+            }
+            Console.WriteLine($"Intre anii {y1} si {y2} sunt {n} ani bisecti.");
+            Console.ReadKey();
+
+        }
+    }
+}
