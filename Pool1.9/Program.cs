3986fe87c1f71fff7701bb7b2c0340c8594ee801
Cociuba Nicusor <cociubanicusor66@gmail.com>
2020-12-17 21:19:18 +0200

    ty

---
+++ Pool1.9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Pool1._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Afisati toti divizorii numarului n. 
+            int n, i;
+            Console.WriteLine("Introduceti numarul n:");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Divizorii lui n sunt:");
+            for (i=1;i<=n;i++)
+            if(n%i==0)
+            Console.WriteLine($"{i}");
+            Console.ReadKey();
+            
+        }
+    }
+}
