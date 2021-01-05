5306486fe0084e679da9dacb894410d826e4e4c9
Cociuba Nicusor <cociubanicusor66@gmail.com>
2021-01-05 23:24:27 +0200

    ty

---
+++ Pool1.8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Pool1._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se dau doua variabile numerice a si b ale carori valori sunt date de intrare.
+            //Se cere sa se inverseze valorile lor fara a folosi alte variabile suplimentare. 
+            int a, b;
+            Console.WriteLine("Introduceti numarul a:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti numarul b:");
+            b = int.Parse(Console.ReadLine());
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.WriteLine($"a={a},b={b}");
+            Console.ReadKey();
+        }
+    }
+}
