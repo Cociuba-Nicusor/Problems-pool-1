20bf9bda636158f646084d91c4317a7fb89333b7
Cociuba Nicusor <cociubanicusor66@gmail.com>
2021-01-05 22:46:10 +0200

    ty

---
+++ Pool1.17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pool1._17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Determianti cel mai mare divizor comun si cel mai mic multiplu comun a doua numere. Folositi algoritmul lui Euclid.
+            int a, b, p;
+            Console.WriteLine("Introduceti primul numar:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti al doilea numar:");
+            b = int.Parse(Console.ReadLine());
+            p = a * b;
+            while (a != b)
+            {
+                if (a > b)
+                    a = a - b;
+                if (b > a)
+                    b = b - a;
+            }
+            Console.WriteLine($"Cel mai mare divizor comun a celor doua numere este {a}.");
+            Console.WriteLine($"Cel mai mic multiplu comun a celor doua numere este {p/ a}");
+            Console.ReadKey();
+        }
+    }
+}
