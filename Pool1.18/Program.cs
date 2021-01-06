98903f2ae44fa0b6e841bb5e2d0a5fd071dcf4c1
Cociuba Nicusor <cociubanicusor66@gmail.com>
2021-01-06 21:34:45 +0200

    ty

---
+++ Pool1.18/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Pool1._18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Afisati descompunerea in factori primi ai unui numar n.
+            int n, exp = 0, div = 2;
+            Console.WriteLine("Introduceti numarul n:");
+            n = int.Parse(Console.ReadLine());
+            while (n != 1)
+            {
+                while (n % div == 0)
+                {
+                    exp++;
+                    n = n / div;
+                }
+
+                if (exp != 0)
+                {
+                    Console.WriteLine($"{div} ^ {exp}");
+                    div=div+1;
+                    exp = 0;
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
