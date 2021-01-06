93f041ef055a27cafb00b9f6e55ccc01e6df4547
Cociuba Nicusor <cociubanicusor66@gmail.com>
2021-01-06 21:48:15 +0200

    ty

---
+++ Pool1.11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Pool1._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Afisati in ordine inversa cifrele unui numar n. 
+            int n, ogl=0, cif;
+            Console.WriteLine("Introduceti numarul n:");
+            n = int.Parse(Console.ReadLine());
+            while(n!=0)
+            {
+                cif = n % 10;
+                ogl = ogl * 10 + cif;
+                n = n / 10;
+                Console.Write($"{cif}");
+            }
+            
+            Console.ReadKey();  
+        }
+    }
+}
